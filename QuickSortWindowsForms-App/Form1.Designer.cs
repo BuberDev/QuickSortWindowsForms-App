5f7a2905ab6371a46a169d99832eb06b820bb819
Dawid Bubernak <dbu@ge.gefasoft-engineering.de>
2022-09-22 15:42:51 +0200

    Projektdateien hinzufügen.

---
+++ QuickSortWindowsForms-App/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace QuickSortWindowsForms_App
+{
+    partial class Quicksort
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UnsortedList = new System.Windows.Forms.ListBox();
+            this.SortedList = new System.Windows.Forms.ListBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.doQuicksortBtn = new System.Windows.Forms.Button();
+            this.numberToAdd = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clearBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numberToAdd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UnsortedList
+            // 
+            this.UnsortedList.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UnsortedList.FormattingEnabled = true;
+            this.UnsortedList.ItemHeight = 23;
+            this.UnsortedList.Location = new System.Drawing.Point(16, 200);
+            this.UnsortedList.Name = "UnsortedList";
+            this.UnsortedList.Size = new System.Drawing.Size(160, 510);
+            this.UnsortedList.TabIndex = 0;
+            // 
+            // SortedList
+            // 
+            this.SortedList.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SortedList.FormattingEnabled = true;
+            this.SortedList.ItemHeight = 27;
+            this.SortedList.Location = new System.Drawing.Point(392, 193);
+            this.SortedList.Name = "SortedList";
+            this.SortedList.Size = new System.Drawing.Size(171, 517);
+            this.SortedList.TabIndex = 1;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.addBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBtn.Location = new System.Drawing.Point(55, 117);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(80, 54);
+            this.addBtn.TabIndex = 2;
+            this.addBtn.Text = "ADD";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // doQuicksortBtn
+            // 
+            this.doQuicksortBtn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.doQuicksortBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.doQuicksortBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.doQuicksortBtn.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.doQuicksortBtn.Location = new System.Drawing.Point(198, 316);
+            this.doQuicksortBtn.Name = "doQuicksortBtn";
+            this.doQuicksortBtn.Size = new System.Drawing.Size(171, 95);
+            this.doQuicksortBtn.TabIndex = 3;
+            this.doQuicksortBtn.Text = "DO QUICKSORT";
+            this.doQuicksortBtn.UseVisualStyleBackColor = false;
+            this.doQuicksortBtn.Click += new System.EventHandler(this.doQuicksortBtn_Click);
+            // 
+            // numberToAdd
+            // 
+            this.numberToAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numberToAdd.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberToAdd.Location = new System.Drawing.Point(36, 46);
+            this.numberToAdd.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numberToAdd.Name = "numberToAdd";
+            this.numberToAdd.Size = new System.Drawing.Size(136, 54);
+            this.numberToAdd.TabIndex = 4;
+            this.numberToAdd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numberToAdd.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(372, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 34);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "QUICKSORT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter your numbers";
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(225, 456);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(109, 55);
+            this.clearBtn.TabIndex = 7;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Quicksort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 781);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numberToAdd);
+            this.Controls.Add(this.doQuicksortBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.SortedList);
+            this.Controls.Add(this.UnsortedList);
+            this.Name = "Quicksort";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numberToAdd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox UnsortedList;
+        private System.Windows.Forms.ListBox SortedList;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button doQuicksortBtn;
+        private System.Windows.Forms.NumericUpDown numberToAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ QuickSortWindowsForms-App/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickSortWindowsForms_App
+{
+    public partial class Quicksort : Form
+    {
+        //Initziere eine Liste
+        private List<int> numbers = new List<int>();
+        public Quicksort()
+        {
+            InitializeComponent();
+        }
+
+        //"addBtn" hat ein Click Event und fügt eine Zahlen in die "UnsortedList" zu
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            int number = Convert.ToInt32(numberToAdd.Value);
+            UnsortedList.Items.Add(numberToAdd.Value);
+            numbers.Add(number);
+         
+            
+        }
+        //"doQuicksortBtn" hat ein ClickEvent. Der macht ganze Quicksort
+        private void doQuicksortBtn_Click(object sender, EventArgs e)
+        {
+            //Ausrufen 
+            Quick_Sort(numbers, 0, numbers.Count -1);
+            Partition(numbers, 0, numbers.Count -1);
+
+            //Die schleife zeigt sortierte Liste 
+            foreach(int num in numbers)
+            {
+                SortedList.Items.Add(num);
+            }
+        }
+        //In dieser Funktion geht es um Rekursion
+        public static void Quick_Sort(List<int> numbers, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(numbers, left, right);
+
+                if (pivot > 1)
+                {
+                    //Linke Bereich vom Pivot
+                    Quick_Sort(numbers, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {   //Recht Bereich vom Pivot
+                    Quick_Sort(numbers, pivot + 1, right);
+                }
+            }
+
+        }
+
+        public static int Partition(List<int> numbers, int leftPartition, int rightPartition)
+        {
+            float temp1 = ((leftPartition + rightPartition) / 2);
+            //Erstellen eine Pivot-variable. Am besten in der Mitte. Es kann erste position oder auch letzte sein
+            int pivot = numbers[Convert.ToInt32(Math.Round(temp1))];
+            
+            while (true)
+            {
+                //Prüfen ob Wert auf erste Position kleiner ist als pivot. Wenn ja,
+                //dann wir prüfen Wert eine Position weiter
+                while (numbers[leftPartition] < pivot)
+                {
+                    leftPartition++;
+                }
+                //Prüfen ob Wert auf letzte Position kleiner ist als pivot. Wenn ja,
+                //dann wir prüfen Wert eine vorherige Position.  
+                while (numbers[rightPartition] > pivot)
+                {
+                    rightPartition--;
+                }
+
+                if (leftPartition < rightPartition)
+                {
+                    if (numbers[leftPartition] == numbers[rightPartition])
+                    {
+                        return rightPartition;
+                    }
+                    //Wir vertauschen Wert "numbers[left]" mit den Wert "numbers[right]"
+                    //Wir inizieren eien Hilfsvariable temp in der wir könenen vordesten Wert zwischen speichern
+                    int temp = numbers[leftPartition];
+                    numbers[leftPartition] = numbers[rightPartition];
+                    numbers[rightPartition] = temp;
+
+
+                }
+                else
+                {
+                    return rightPartition;
+                }
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            UnsortedList.Items.Clear();
+            SortedList.Items.Clear();
+            numbers.Clear();
+        }
+    }
+}
